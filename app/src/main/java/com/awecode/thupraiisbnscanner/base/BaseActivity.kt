8adb6ddda5fe3faa6dfcf2be5ff8d93cba30bcdf
munnadroid <gotameraju2@gmail.com>
2019-01-26 23:19:47 +0545

    add rxpermission

--- app/src/main/java/com/awecode/thupraiisbnscanner/base/BaseActivity.kt
+++ app/src/main/java/com/awecode/thupraiisbnscanner/base/BaseActivity.kt
@@ -2,13 +2,18 @@ package com.awecode.thupraiisbnscanner.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tbruyelle.rxpermissions2.RxPermissions
+
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    var rxPermissions: RxPermissions? = null;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutId)
-
+        rxPermissions = RxPermissions(this)
         initView()
     }
 
